31a8e5e8c61d6f7235cadb31fc11698bd75b1191
Evelyn <evelynbrandao15@gmail.com>
2021-01-06 12:30:02 -0300

    Listagem de cards de produtos

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,11 +1,24 @@
 import "./styles.css";
 // import { ReactComponent as Logo } from "./logo.svg";
 import StepsHeader from "./StepsHeader";
+import ProductsList from "./ProductsList";
+import { Product } from "./types";
+import { useEffect, useState } from "react";
+import { fetchProducts } from "../api";
 
 function Orders() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    fetchProducts()
+      .then((response) => setProducts(response.data))
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <div className="orders-container">
       <StepsHeader />
+      <ProductsList products={products} />
     </div>
   );
 }
