1609a248a78c77929fd0f1522419efed13591a80
Evelyn <evelynbrandao15@gmail.com>
2021-01-08 17:56:58 -0300

    Links in icon

--- front-web/src/Footer/index.tsx
+++ front-web/src/Footer/index.tsx
@@ -8,13 +8,13 @@ function Footer() {
     <footer className="main-footer">
       App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
       <div className="footer-icons">
-        <a href="#" target="_new">
+        <a href="https://www.youtube.com/c/DevSuperior" target="_new">
           <YouTubeIcon />
         </a>
-        <a href="#" target="_new">
+        <a href="https://www.linkedin.com/school/devsuperior/" target="_new">
           <LinkedinIcon />
         </a>
-        <a href="#" target="_new">
+        <a href="https://www.instragram.com/devsuperior.ig/" target="_new">
           <InstagramIcon />
         </a>
       </div>
