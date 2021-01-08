eb6579e6ae2d8709a23f85ddb429c844d0950d86
Evelyn <evelynbrandao15@gmail.com>
2021-01-08 12:43:56 -0300

    Finish front-web

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 import Routes from "./Routes";
 
 function App() {
-  return <Routes />;
+  return (
+    <>
+      <Routes />
+      <ToastContainer />
+    </>
+  );
 }
 
 export default App;
